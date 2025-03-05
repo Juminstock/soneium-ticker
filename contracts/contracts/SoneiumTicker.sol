d48927cd80eb3adbefecc726a9a1810acaaed224
Carlos Rodríguez <juminstock@gmail.com>
2025-03-05 01:19:01 -0500

    Update: create hardhat project and SoneiumTicker contract

---
+++ contracts/contracts/SoneiumTicker.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SoneiumTickerLaunch is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    uint256 private _nextTokenId = 1;
+    string private constant METADATA_URI = "https://gateway.pinata.cloud/ipfs/bafkreifymub2uzk3thokevsc3fokwo2dkk4peih6yeyq3p2gluo3bkpgzq";
+
+    constructor(address initialOwner)
+        ERC721("SoneiumTickerLaunch", "STL")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to) public onlyOwner returns (uint256) {
+        require(to != address(0), "Invalid address");
+
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, METADATA_URI);
+
+        return tokenId;
+    }
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,7 +1,22 @@
-import type { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
+  networks: {
+    hardhat: {},
+    minato: {
+      url: "https://soneium-minato.g.alchemy.com/v2/etZm12pLQ8B0KzV50lKmYKOsWbAW_hX5",
+      chainId: 1946,
+      from: "0x78b4fdd087eC725fb18c5fbdA4471FD1d766ba3F",
+      accounts: ["6150c316687bf456dd7c868f7f2b65f48e618e2a1e745659cdcc916ddfc5ff53"]
+    },
+    soneium : {
+      url: "https://soneium-mainnet.g.alchemy.com/v2/etZm12pLQ8B0KzV50lKmYKOsWbAW_hX5",
+      chainId: 1868,
+      from: "0x78b4fdd087eC725fb18c5fbdA4471FD1d766ba3F",
+      accounts: ["6150c316687bf456dd7c868f7f2b65f48e618e2a1e745659cdcc916ddfc5ff53"]
+    }
+  },
   solidity: "0.8.28",
 };
 
